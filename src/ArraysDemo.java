5cf8c7fdc062b3f4c2d78f809bec5048cd6f247c
hai1324 <23000115632@qq.com>
2026-09-09 22:05:05 +0800

    update

--- src/ArraysDemo.java
+++ src/ArraysDemo.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class ArraysDemo {
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ArraysDemo {
+    public static void main(String[] args) {
+        Integer[] arr ={1,2,3,4,58,4,3,2,5,2,7};
+        //插入排序和二分查找
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+//        Arrays.sort(arr, (Integer o1, Integer o2)->o2-o1);//lambda极简写法
+        System.out.println(Arrays.toString(arr));
+        Integer[] arrtwo=Arrays.copyOf(arr, 10);
+        System.out.println(Arrays.toString(arrtwo));
+        Integer[] arrthree=Arrays.copyOfRange(arr, 0, 5);
+        System.out.println(Arrays.toString(arrthree));
+        System.out.println(Arrays.binarySearch(arr, 5));
+        System.out.println(Arrays.binarySearch(arr, 0, 5, 5));
+        Arrays.fill(arr, 0);
+        System.out.println(Arrays.toString(arr));
+        Arrays.fill(arr, 0, 5, 1);
+        System.out.println(Arrays.toString(arr));
+        Arrays.parallelSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+//    1==1
+//    2==2
+    //    3==1+2=3,(111,12,21)
 }
--- src/CollectionDemo.java
+++ src/CollectionDemo.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class CollectionDemo {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class CollectionDemo {
+    public static void main(String[] args) {
+        //创建集合对象
+        Collection<String> c = new ArrayList<>();
+
+        //添加元素
+        c.add("hello");
+        c.add("world");
+        c.add("java");
+        c.add("javaee");
+
+        //Iterator<E> iterator()：返回此集合中元素的迭代器，通过集合的iterator()方法得到
+        Iterator<String> it = c.iterator();
+
+        //用while循环改进元素的判断和获取
+        while (it.hasNext()) {
+            String s = it.next();
+            System.out.println(s);
+        }
+        ArrayList<String> list =  new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.add("e");
+        list.add("f");
+
+        //1,数据类型一定是集合或者数组中元素的类型
+        //2,str仅仅是一个变量名而已,在循环的过程中,依次表示集合或者数组中的每一个元素
+        //3,list就是要遍历的集合或者数组
+        for(String str : list){
+            System.out.println(str);
+        }
+    }
 }
